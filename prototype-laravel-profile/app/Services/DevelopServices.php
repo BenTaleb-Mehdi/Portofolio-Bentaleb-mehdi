c89eee25c5fff57474e5b388a06883010b35a350
Mehdy <mehdibentaleb548@gmail.com>
2025-10-23 15:40:18 +0100

    feat: setup  structure

---
+++ prototype-laravel-profile/app/Services/DevelopServices.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Services;
+
+
+class DevelopServices {
+    public function getProfile(){
+        return [
+             'name' => 'Mehdi BenTaleb',
+            'role' => 'Web developer',
+            'bio' => 'Mehdi bentaleb developer full-stack i have 2 Years Experinces  ',
+            'Email' => 'Mehdibentaleb548@gmail.com'
+        ];
+    }
+}
